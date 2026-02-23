a9cdc2a3d70ec8705280977909ab26c08f30de57
januarcexp <mccoystar1818@gmail.com>
2026-02-23 14:56:08 +0700

    update 3

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,7 +3,18 @@ import { test, expect } from '@playwright/test';
 test('test bukalapak', async ({ page }) => {
   await page.goto('https://www.bukalapak.com/');
   await page.waitForLoadState('networkidle');
-  await page.getByRole('link', { name: 'Transaksi' }).click();
-  await page.locator('a').filter({ hasText: 'Produk VirtualRiwayat' }).click();
-  await page.getByRole('link', { name: 'Daftar' }).click();
+  
+  const transaksiLink = page.getByRole('link', { name: 'Transaksi' });
+  await expect(transaksiLink).toBeVisible({ timeout: 10000 });
+  await transaksiLink.click();
+
+  const produkLink = page.locator('a', { hasText: 'Produk VirtualRiwayat' });
+  await expect(produkLink).toBeVisible({ timeout: 10000 });
+  produkLink.click();
+
+  const daftarLink = page.getByRole('link', { name: 'Daftar' });
+  await expect(daftarLink).toBeVisible({ timeout: 10000 });
+  await daftarLink.click();
 });
+
+
