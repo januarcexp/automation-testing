4cd9ffb33fb4140770ee3cc4d9e1618c810ae1ba
januarcexp <mccoystar1818@gmail.com>
2026-02-23 14:43:37 +0700

    update 2

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test bukalapak', async ({ page }) => {
   await page.goto('https://www.bukalapak.com/');
+  await page.waitForLoadState('networkidle');
   await page.getByRole('link', { name: 'Transaksi' }).click();
   await page.locator('a').filter({ hasText: 'Produk VirtualRiwayat' }).click();
   await page.getByRole('link', { name: 'Daftar' }).click();
